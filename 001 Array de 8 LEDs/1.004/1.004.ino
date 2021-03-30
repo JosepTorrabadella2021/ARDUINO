a0a90675abb7cc7de8b4ab9f984c087374be3892
joseptorrabadella2021 <josep.torrabadella.60@lacetania.cat>
2021-03-30 15:27:47 +0200

    vaa

--- 001 Array de 8 LEDs/1.004/1.004.ino
+++ 001 Array de 8 LEDs/1.004/1.004.ino
@@ -1,170 +1,34 @@
 /**********************************************************************************
 **                                                                               **
-**                              Array de 8 LEDs                                  **
+**                           on/off button control LED                           **
 **                                                                               **
 **                                                                               **
 **********************************************************************************/
-
 //********** Includes *************************************************************
-
 //********** Variables ************************************************************
 const int led0 = 5;          // donar nom al pin 5 de l’Arduino
-const int led1 = 6;          // donar nom al pin 6 de l’Arduino
-const int led2 = 7;          // donar nom al pin 7 de l’Arduino
-const int led3 = 8;          // donar nom al pin 8 de l’Arduino
-const int led4 = 9;          // donar nom al pin 9 de l’Arduino
-const int led5 = 10;         // donar nom al pin 10 de l’Arduino
-const int led6 = 11;         // donar nom al pin 11 de l’Arduino
-const int led7 = 12;         // donar nom al pin 12 de l’Arduino
-unsigned long temps = 300;
-
+const int buttonPin = 2;     // donar nom al pin 2 de l’Arduino
+int buttonState = 0;         // per guardar l’estat en que és troba el button
 //********** Setup ****************************************************************
 void setup()
 {
   pinMode(led0, OUTPUT);     // definir el pin 5 com una sortida
-  pinMode(led1, OUTPUT);     // definir el pin 6 com una sortida
-  pinMode(led2, OUTPUT);     // definir el pin 7 com una sortida
-  pinMode(led3, OUTPUT);     // definir el pin 8 com una sortida
-  pinMode(led4, OUTPUT);     // definir el pin 9 com una sortida
-  pinMode(led5, OUTPUT);     // definir el pin 10 com una sortida
-  pinMode(led6, OUTPUT);     // definir el pin 11 com una sortida
-  pinMode(led7, OUTPUT);     // definir el pin 12 com una sortida
+  pinMode(buttonPin, INPUT); // definir el pin 2 com una entrada  
 }
-
 //********** Loop *****************************************************************
 void loop()
 {
-  digitalWrite(led0, HIGH);     
-  digitalWrite(led1, LOW);    
-  digitalWrite(led2, LOW);    
-  digitalWrite(led3, LOW);    
-  digitalWrite(led4, LOW);    
-  digitalWrite(led5, LOW);    
-  digitalWrite(led6, LOW);    
-  digitalWrite(led7, HIGH);    
-  
-  delay(temps);                  
-  
-  digitalWrite(led0, HIGH);     
-  digitalWrite(led1, HIGH);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, HIGH);     
-  digitalWrite(led7, HIGH);     
-  
-  delay(temps);                  
+        buttonState = digitalRead(buttonPin);  //llegir l’estat del button i gardar-lo
+  if (buttonState == 0)
+  {
+  digitalWrite(led0, HIGH);
 
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, HIGH);     
-  digitalWrite(led2, HIGH);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, HIGH);     
-  digitalWrite(led6, HIGH);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
-  
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, HIGH);     
-  digitalWrite(led3, HIGH);     
-  digitalWrite(led4, HIGH);     
-  digitalWrite(led5, HIGH);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
-  
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, HIGH);     
-  digitalWrite(led4, HIGH);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
+ }
+ else
+ {
+  digitalWrite(led0, LOW);
 
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, HIGH);     
-  digitalWrite(led4, HIGH);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
-
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, HIGH);     
-  digitalWrite(led3, HIGH);     
-  digitalWrite(led4, HIGH);     
-  digitalWrite(led5, HIGH);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
-
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, HIGH);     
-  digitalWrite(led2, HIGH);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, HIGH);     
-  digitalWrite(led6, HIGH);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
-
-  digitalWrite(led0, HIGH);     
-  digitalWrite(led1, HIGH);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, HIGH);     
-  digitalWrite(led7, HIGH);     
-  
-  delay(temps);                  
-
-  digitalWrite(led0, HIGH);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, HIGH);     
-  
-  delay(temps);                  
-
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
-
-  digitalWrite(led0, LOW);     
-  digitalWrite(led1, LOW);     
-  digitalWrite(led2, LOW);     
-  digitalWrite(led3, LOW);     
-  digitalWrite(led4, LOW);     
-  digitalWrite(led5, LOW);     
-  digitalWrite(led6, LOW);     
-  digitalWrite(led7, LOW);     
-  
-  delay(temps);                  
+ }
+delay(200);  //per no estar llegint button molt ràpid
 }
-
 //********** Funcions *************************************************************
