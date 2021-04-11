ec9a6f3662251f26803aa44321ba3ed23d44fe47
joseptorrabadella2021 <josep.torrabadella.60@lacetania.cat>
2021-04-11 17:45:05 +0200

    DISPLAY

---
+++ DISPLAY/001/NOM/NOM.ino
@@ -0,0 +1,95 @@
+/**********************************************************************************
+**                                                                               **
+**                              NOM                                              **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const int ledA = 5;          // donar nom al pin 5 de l’Arduino
+const int ledB = 6;          // donar nom al pin 6 de l’Arduino
+const int ledC = 7;          // donar nom al pin 7 de l’Arduino
+const int ledD = 8;          // donar nom al pin 8 de l’Arduino
+const int ledE = 9;          // donar nom al pin 9 de l’Arduino
+const int ledF = 10;         // donar nom al pin 10 de l’Arduino
+const int ledG = 11;         // donar nom al pin 11 de l’Arduino
+const int ledH = 12;         // donar nom al pin 12 de l’Arduino
+unsigned long temps1=1000;                             //zona temporal 1
+
+
+//********** Setup ****************************************************************
+
+  void setup() {
+  Serial.begin(9600);                               //definir velocitat
+  
+  pinMode(ledA, OUTPUT);     // definir el pin 5 com una sortida
+  pinMode(ledB, OUTPUT);     // definir el pin 6 com una sortida
+  pinMode(ledC, OUTPUT);     // definir el pin 7 com una sortida
+  pinMode(ledD, OUTPUT);     // definir el pin 8 com una sortida
+  pinMode(ledE, OUTPUT);     // definir el pin 9 com una sortida
+  pinMode(ledF, OUTPUT);     // definir el pin 10 com una sortida
+  pinMode(ledG, OUTPUT);     // definir el pin 11 com una sortida
+  
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  digitalWrite(ledA, LOW);    
+  digitalWrite(ledB, HIGH);    
+  digitalWrite(ledC, HIGH);    
+  digitalWrite(ledD, HIGH);    
+  digitalWrite(ledE, HIGH);    
+  digitalWrite(ledF, LOW);    
+  digitalWrite(ledG, LOW);   
+
+  delay (temps1);
+
+  digitalWrite(ledA, HIGH);    
+  digitalWrite(ledB, HIGH);    
+  digitalWrite(ledC, HIGH);    
+  digitalWrite(ledD, HIGH);    
+  digitalWrite(ledE, HIGH);    
+  digitalWrite(ledF, HIGH);    
+  digitalWrite(ledG, LOW);   
+
+  delay (temps1);
+
+  digitalWrite(ledA, HIGH);    
+  digitalWrite(ledB, LOW);    
+  digitalWrite(ledC, HIGH);    
+  digitalWrite(ledD, HIGH);    
+  digitalWrite(ledE, LOW);    
+  digitalWrite(ledF, HIGH);    
+  digitalWrite(ledG, HIGH);   
+
+  delay (temps1);
+
+  digitalWrite(ledA, HIGH);    
+  digitalWrite(ledB, LOW);    
+  digitalWrite(ledC, LOW);    
+  digitalWrite(ledD, HIGH);    
+  digitalWrite(ledE, HIGH);    
+  digitalWrite(ledF, HIGH);    
+  digitalWrite(ledG, HIGH);   
+
+  delay (temps1);
+
+  digitalWrite(ledA, HIGH);    
+  digitalWrite(ledB, HIGH);    
+  digitalWrite(ledC, LOW);    
+  digitalWrite(ledD, LOW);    
+  digitalWrite(ledE, HIGH);    
+  digitalWrite(ledF, HIGH);    
+  digitalWrite(ledG, HIGH);   
+
+  delay (temps1);
+
+  
+
+  
+  
+
+}
---
+++ DISPLAY/004/COMPTADOR_0_9/COMPTADOR_0_9.ino
@@ -0,0 +1,153 @@
+/**********************************************************************************
+**                                                                               **
+**                              Array de 8 LEDs                                  **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+const int ledPin[] = {5, 6, 7, 8, 9, 10, 11};   // donar nom en un array als pins 
+const int buttonPin = 2;             // donar nom al pin 2 de l’Arduino
+boolean buttonEstat = LOW;           // definir variable d'estat pel polsador
+unsigned long temps1=1000;
+int ledNum = 7;                   // definir variable de número de leds
+
+
+//********** Setup ****************************************************************
+void setup()
+{
+  for(int i = 0; i < ledNum; i++) // definir els pins com sortides
+  {
+    pinMode(ledPin[i], OUTPUT);
+  }
+  pinMode(buttonPin, INPUT);      // definir el pin 2 com una entrada
+  
+  for(int j = 0; j < ledNum ; j++)     // els leds mostren incialment 0
+    { 
+      digitalWrite(ledPin[j], 0);
+    }
+}
+
+//********** Loop *****************************************************************
+void loop()
+{
+  buttonEstat = digitalRead(buttonPin);
+  if (buttonEstat == HIGH)
+  { 
+  digitalWrite(5,0);    
+  digitalWrite(6,0);    
+  digitalWrite(7,0);    
+  digitalWrite(8,0);    
+  digitalWrite(9,0);    
+  digitalWrite(10,0);    
+  digitalWrite(11,0);   
+
+  delay (temps1);
+    
+  digitalWrite(5,0);    
+  digitalWrite(6,1);    
+  digitalWrite(7,1);    
+  digitalWrite(8,0);    
+  digitalWrite(9,0);    
+  digitalWrite(10,0);    
+  digitalWrite(11,0);   
+
+  delay (temps1);
+
+  digitalWrite(5,1);    
+  digitalWrite(6,1);    
+  digitalWrite(7,0);    
+  digitalWrite(8,1);    
+  digitalWrite(9,1);    
+  digitalWrite(10,0);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  digitalWrite(5,1);    
+  digitalWrite(6,1);    
+  digitalWrite(7,1);    
+  digitalWrite(8,1);    
+  digitalWrite(9,0);    
+  digitalWrite(10,0);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  digitalWrite(5,0);    
+  digitalWrite(6,1);    
+  digitalWrite(7,1);    
+  digitalWrite(8,0);    
+  digitalWrite(9,0);    
+  digitalWrite(10,1);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  digitalWrite(5,1);    
+  digitalWrite(6,0);    
+  digitalWrite(7,1);    
+  digitalWrite(8,1);    
+  digitalWrite(9,0);    
+  digitalWrite(10,1);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  digitalWrite(5,0);    
+  digitalWrite(6,0);    
+  digitalWrite(7,1);    
+  digitalWrite(8,1);    
+  digitalWrite(9,1);    
+  digitalWrite(10,1);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  digitalWrite(5,1);    
+  digitalWrite(6,1);    
+  digitalWrite(7,1);    
+  digitalWrite(8,0);    
+  digitalWrite(9,0);    
+  digitalWrite(10,0);    
+  digitalWrite(11,0);   
+
+  delay (temps1);
+
+  digitalWrite(5,1);    
+  digitalWrite(6,1);    
+  digitalWrite(7,1);    
+  digitalWrite(8,1);    
+  digitalWrite(9,1);    
+  digitalWrite(10,1);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  digitalWrite(5,1);    
+  digitalWrite(6,1);    
+  digitalWrite(7,1);    
+  digitalWrite(8,0);    
+  digitalWrite(9,0);    
+  digitalWrite(10,1);    
+  digitalWrite(11,1);   
+
+  delay (temps1);
+
+  
+  } 
+
+  else (buttonEstat == LOW);
+    {
+  digitalWrite(5,0);    
+  digitalWrite(6,0);    
+  digitalWrite(7,0);    
+  digitalWrite(8,0);    
+  digitalWrite(9,0);    
+  digitalWrite(10,0);    
+  digitalWrite(11,0); 
+  }
+  }   
+//********** Funcions *************************************************************
